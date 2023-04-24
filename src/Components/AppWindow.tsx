d884eee745464de68a228c7b189dfb3e010fb98a
Jupi <spacejupiter0@gmail.com>
2023-04-24 19:10:17 +0200

    Added window movement

--- src/Components/AppWindow.tsx
+++ src/Components/AppWindow.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useState } from "react";
 import "../Styles/AppWindow.css";
 import { AnimatePresence, motion } from "framer-motion";
+import useMousePosition from "../Hooks/useMousePosition";
 
 type CloseAppFunction = () => void;
 
@@ -9,10 +10,15 @@ type PropType = {
     CloseApp?: CloseAppFunction
 }
 
+let MoveWindow = false;
+let LastMouseLocation: {x: number, y: number} = {x: 0, y: 0};
+
 function AppWindow(props: PropType)
 {
     const [Maximized, SetMaximized] = useState(false);
 
+    const CursorLocation = useMousePosition();
+    
     function MaximizeWindow()
     {
         SetMaximized(!Maximized);
@@ -35,6 +41,8 @@ function AppWindow(props: PropType)
         visible: {
             scale: 1,
             filter: "blur(0px)",
+            x: Maximized? 0 : LastMouseLocation.x,
+            y: Maximized? 0 : LastMouseLocation.y,
             opacity: 1,
             width: Maximized? "100%" : undefined,
             height: Maximized? "100%" : undefined,
@@ -43,25 +51,47 @@ function AppWindow(props: PropType)
         },
     }
     
-    return <motion.div className={`app-window${MaximizedClass}`}
+    function onMouseDown()
+    {
+        MoveWindow = true;
+    }
+
+    function onMouseUp()
+    {
+        StopMovingWindow();
+    }
+
+    function StopMovingWindow()
+    {
+        MoveWindow = false;
+    }
+
+    if (MoveWindow)
+    {
+        LastMouseLocation = {
+            x: CursorLocation.x === null? 0 : CursorLocation.x - 150,
+            y: CursorLocation.y === null? 0 : CursorLocation.y - 10 
+        };
+    }
+    
+    return (
+        <motion.div className={`app-window${MaximizedClass}`}
             variants={exitAndOpen}
             initial="hidden"
             animate="visible"
-            transition={{duration: 0.1, width: {duration: 0.125}, height: {duration: 0.125}}}
-            layout
-            >
-            <div className="window-header">
-                <div className="window-controls">
-                    <span className="window-control-button window-close-button" onClick={CloseApplication}>✖</span>
-                    <span className="window-control-button" onClick={MaximizeWindow}><span id="square-button"></span></span>
-                    <span className="window-control-button"><span id="dismiss-button"/></span>
+            transition={{duration: 0.1, width: {duration: 0.125}, height: {duration: 0.125}, x: {duration: 0}, y: {duration: 0}}} >
+                <div className="window-header" onMouseDown={onMouseDown} onMouseUp={onMouseUp}>
+                    <div className="window-controls">
+                        <span className="window-control-button window-close-button" onClick={CloseApplication}>✖</span>
+                        <span className="window-control-button" onClick={MaximizeWindow}><span id="square-button"></span></span>
+                        <span className="window-control-button"><span id="dismiss-button"/></span>
+                    </div>
                 </div>
-            </div>
-            <div className="window-content">
-                {props.children}
-            </div>
+                <div className="window-content">
+                    {props.children}
+                </div>
         </motion.div>
-    
+    )
 }
 
 export default AppWindow;
