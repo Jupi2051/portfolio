3075c4d3478e8497f6876e2d9488955e4f88394b
Jupi <spacejupiter0@gmail.com>
2023-04-26 17:31:42 +0200

    windows settings styling

--- src/Components/WindowsSettings.tsx
+++ src/Components/WindowsSettings.tsx
@@ -1,9 +1,9 @@
 import { motion } from "framer-motion";
 import "../Styles/WindowsSettings.css";
 
-const AnimationVariants = {
+export const BottomAnimationVariants = {
     hidden: {
-        y: 200,
+        y: "100vh",
         x: "-50%"
     },
     visible: {
@@ -11,17 +11,16 @@ const AnimationVariants = {
         x: "-50%"
     },
     exit: {
-        y: 200,
+        y: "100vh",
         x: "-50%"
     }
 }
 
 function WindowsSettings()
 {
-    return <motion.div className="windows-settings-container" variants={AnimationVariants} initial="hidden" animate="visible" exit="exit">
-        <h1>asfoasfka</h1>
+    return <motion.div className="windows-settings-container" variants={BottomAnimationVariants} initial="hidden" animate="visible" exit="exit">
+        <h1>Elemen</h1>
         <h2>asfasosfkasofkaos</h2>
     </motion.div>
 }
-
 export default WindowsSettings;
