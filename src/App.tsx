1563a9d72c3d82d00f1bba6e3b68cfe5dbae4bbc
Jupi <spacejupiter0@gmail.com>
2023-04-30 02:25:08 +0300

    some tidying

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 import Surface from './Components/Surface'
-import Taskbar from './Components/Taskbar'
 
 function App() {
   return (
