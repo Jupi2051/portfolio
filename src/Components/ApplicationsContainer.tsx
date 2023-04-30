142e7209e86b22d1fb4914de86b32d19935fc812
Jupi <spacejupiter0@gmail.com>
2023-04-30 03:04:55 +0300

    added eien project app

--- src/Components/ApplicationsContainer.tsx
+++ src/Components/ApplicationsContainer.tsx
@@ -4,6 +4,7 @@ import { OpenApplication } from "./Desktop";
 import DummyApp from "./Apps/DummyApp";
 import Jenni from "./Apps/Jenni";
 import Photos from "./Apps/Photos";
+import EIEN from "./Apps/EIEN";
 
 type PropTypes = {
     OpenApplications: OpenApplication[],
@@ -12,6 +13,7 @@ type PropTypes = {
 export enum DesktopAppsList {
     DummyApp,
     Jenni,
+    EIEN,
     Photos
 };
 
@@ -24,6 +26,7 @@ function ApplicationsContainer(Props: PropTypes)
                 {
                     case DesktopAppsList.DummyApp: return <DummyApp AppId={openApp.id} key={openApp.id} processIcon={openApp.processIcon} processName={openApp.processName}/>
                     case DesktopAppsList.Jenni: return <Jenni AppId={openApp.id} key={openApp.id} processIcon={openApp.processIcon} processName={openApp.processName}/>
+                    case DesktopAppsList.EIEN: return <EIEN AppId={openApp.id} key={openApp.id} processIcon={openApp.processIcon} processName={openApp.processName}/>
                     case DesktopAppsList.Photos: return <Photos AppId={openApp.id} key={openApp.id} processIcon={openApp.processIcon} processName={openApp.processName} processData={{...openApp.processProps?? {}}} />
                 }
             }
