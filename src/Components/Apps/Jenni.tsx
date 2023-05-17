d45d31538fc6229448bb3a060d49f41bf3af7c96
Jupi <spacejupiter0@gmail.com>
2023-05-17 13:29:00 +0300

    updated fonts

--- src/Components/Apps/Jenni.tsx
+++ src/Components/Apps/Jenni.tsx
@@ -23,15 +23,9 @@ function Jenni(Props: PropTypes)
             <AppWindow AppId={Props.AppId} processIcon={Props.processIcon} processName={Props.processName}>
                 <div style={{width: "100%", height: "100%", border: "none", overflowY: "scroll"}} className="showcase-app-main-container">
                     <div>
-                        <h1 className="showcase-app-header-title text-align-left">Jenni</h1>
+                        <h1 className="showcase-app-header-title text-align-left">Where as a common understanding and freedoms</h1>
                         <Stack StackTools={[StackTool.HTML, StackTool.CSS, StackTool.JS, StackTool.Express, StackTool.Discord, StackTool.NodeJS, StackTool.Ubuntu, StackTool.Nginx, StackTool.JSON]} />
-                        <p className="showcase-app-paragraph">This project was a fully functional website that i built for a digital artist.</p>
                     </div>
-                    
-                    <iframe src="https://www.jennixdraws.com/" style={{width: "70%", height: "70%", border: "none"}}></iframe>                    
-                    <h1>This is the part where i talk about the portfolio</h1>
-                    <p>Yes this is the part where i make even more description about the portfolio Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ipsum enim ullam totam minus, doloribus molestias nulla soluta! Ipsa culpa molestias neque sequi quas velit ipsam tempora nemo aliquid excepturi?</p>
-                    <iframe src="https://www.jennixdraws.com/portfolio" style={{width: "70%", height: "70%", border: "none"}}></iframe>                    
 
                 </div>
             </AppWindow>
