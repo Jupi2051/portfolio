b2132bcc0186295daf9ff16628ad8fca3dfd7336
Jupi <spacejupiter0@gmail.com>
2023-06-30 09:22:54 +0300

    added jenni site

--- src/Components/Apps/Jenni.tsx
+++ src/Components/Apps/Jenni.tsx
@@ -21,12 +21,9 @@ function Jenni(Props: PropTypes)
     return(
         <motion.div variants={exitAndOpen} exit="exit" transition={{duration: 0.1}} initial="init" animate="init" className="main-app-container">
             <AppWindow AppId={Props.AppId} processIcon={Props.processIcon} processName={Props.processName}>
-                <div style={{width: "100%", height: "100%", border: "none", overflowY: "scroll"}} className="showcase-app-main-container">
-                    <div>
-                        <h1 className="showcase-app-header-title text-align-left">Where as a common understanding and freedoms</h1>
-                        <Stack StackTools={[StackTool.HTML, StackTool.CSS, StackTool.JS, StackTool.Express, StackTool.Discord, StackTool.NodeJS, StackTool.Ubuntu, StackTool.Nginx, StackTool.JSON]} />
-                    </div>
-                </div>
+                    {/* <h1 className="showcase-app-header-title text-align-left">Where as a common understanding and freedoms</h1> */}
+                    <Stack StackTools={[StackTool.HTML, StackTool.CSS, StackTool.JS, StackTool.Express, StackTool.Discord, StackTool.NodeJS, StackTool.Ubuntu, StackTool.Nginx, StackTool.JSON]} />
+                    <iframe src="https://www.jennixdraws.com/" style={{width: "100%", height: "100%", border: "none"}}></iframe>
             </AppWindow>
         </motion.div>
     )
