0b6a39297bba4a7c1ae7270c2207a0df6beb4cad
Jupi <spacejupiter0@gmail.com>
2023-07-03 19:06:43 +0300

    updated pinboard styling

--- src/Components/Apps/AppsItems/BoardMessage.tsx
+++ src/Components/Apps/AppsItems/BoardMessage.tsx
@@ -1,15 +1,53 @@
+import { Variants, motion } from "framer-motion";
+import { duration } from "moment";
+
 interface Props {
     name: string,
     content: string
-
 }
+
+const WritingAnimation: Variants = {
+    hidden: {opacity: 0,},
+    visible: {opacity: 1,},
+};
 
 function BoardMessage(props: Props)
 {
-    return <div className="board-message-container">
-        <h1 className="board-message-name">{props.name} :</h1>
-        <p className="board-message-content">{props.content}</p>
-    </div>
+    const contentLetters = props.content.split("");
+    const nameLetters = props.name.split("");
+
+    const StaggerIncrementValue = (props.content.length + props.name.length) * 0.01; // 1% increase per letter
+
+    const ParentMessageAnimation: Variants = {
+        visible: {
+            transition: {
+                duration: 1,
+                staggerChildren: Math.max(0.08 - (StaggerIncrementValue * 0.08), 0.01),
+            }
+        }
+    };
+
+    return <motion.div className="board-message-container" variants={{visible: {transition:{staggerChildren: 0.5, duration: 1}}}}>
+        <motion.h1 className="board-message-name" variants={ParentMessageAnimation}>
+        {
+            nameLetters.map((letter, index) => {
+                return <motion.span variants={WritingAnimation} key={letter+""+index}>
+                    {letter}
+                </motion.span>
+            })
+        }
+        </motion.h1>
+        <motion.span className="board-message-notation">:</motion.span>
+        <motion.p className="board-message-content" variants={ParentMessageAnimation}>
+            {
+                contentLetters.map((letter, index) => {
+                    return <motion.span variants={WritingAnimation} key={letter+""+index}>
+                        {letter}
+                    </motion.span>
+                })
+            }
+        </motion.p>
+    </motion.div>
 }
 
 export default BoardMessage;
--- src/Components/Apps/Social.tsx
+++ src/Components/Apps/Social.tsx
@@ -1,4 +1,4 @@
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, Variants, motion } from "framer-motion";
 import AppWindow from "../AppWindow";
 import Chloe, { ChloeConversation, ChloeEmotion } from "../Chloe";
 import ChloeInfoButton from "../ChloeInfoButton";
@@ -12,6 +12,20 @@ type PropTypes = {
     processName: string,
     processIcon: string
 };
+
+const TitleContainerAnimate: Variants = {
+    visible: {
+        transition: {
+            duration: 1,
+            staggerChildren: 0.08
+        }
+    }
+}
+
+const LetterVariants: Variants = {
+    hidden: {opacity: 0},
+    visible: {opacity: 1}
+}
 
 const exitAndOpen = {
     exit: {opacity: 0,},
@@ -28,7 +42,13 @@ function Social(Props: PropTypes)
 {
     const [content, setContent] = useState("");
     const [name, setName] = useState("");
-    const [messages, setMessages] = useState<Messages[]>([]);
+    const [messages, setMessages] = useState<Messages[]>([
+        {id: 1, content: "burger king", name: "akata"},
+        {id: 2, content: "lorem ipsum dolor isum", name:"Dashie"},
+        {id: 3, content: "ee'e'ee'eee'e", name: "Killian"},
+        {id: 4, content: "if you gotta pee then pee, if you don't then dont", name:"Daddy Syren"},
+    ]);
+    const [backgroundScroll, setBackgroundScroll] = useState<number>(0);
 
     useEffect(() => {
         getMessages()
@@ -46,6 +66,8 @@ function Social(Props: PropTypes)
             event.preventDefault();
             const MessageSend = await sendMessage(name, content);
             setMessages(MessageSend.messages);
+            setName("");
+            setContent("");
         } catch (error) {
             console.log(error);
         }
@@ -60,23 +82,33 @@ function Social(Props: PropTypes)
     {
         setContent(event.currentTarget.value);
     }
+    
+    function onScrollDown(event: React.UIEvent<HTMLDivElement, UIEvent>) {
+        setBackgroundScroll(event.currentTarget.scrollTop);
+    }
 
     return(
         <motion.div variants={exitAndOpen} exit="exit" transition={{duration: 0.1}} initial="init" animate="init" className="main-app-container">
             <AppWindow AppId={Props.AppId} processIcon={Props.processIcon} processName={Props.processName}>
                 <div style={{width: "100%", height: "100%", border: "none"}}>
-                    <div className="messages-board-container">
-                        <div className="Messages">
-                            {
-                                messages.map((element) => <BoardMessage content={element.content} name={element.name} key={element.id}/>)
-                            }
+                    <div className="paper-container">
+                        <div className="messages-board-container" onScroll={onScrollDown} style={{backgroundPositionY: -backgroundScroll+"px"}}>
+                            <div className="messages-container">
+                                <div>
+                                    <h3 className="message-board-title">Jupi's Pinboard</h3>
+                                </div>
+                                <motion.div className="message-writing-container">
+                                    {messages.map((element) => <BoardMessage content={element.content} name={element.name} key={element.id}/>)}
+                                </motion.div>
+                                <img className="cute-drawing" src="/Imgs/Chloe/Excited.webp" />
+                            </div>
                         </div>
                         <form className="message-form">
                             <div className="message-payload">
-                                <input type="text" name="Username" placeholder="name" onChange={onNameChange} value={name}/>
-                                <input type="text" name="Message" placeholder="message..." onChange={onContentChange} value={content}/>
+                                <input className="text-input" type="text" name="Username" placeholder="Your name ..." onChange={onNameChange} value={name} maxLength={20}/>
+                                <input className="text-input" type="text" name="Message" placeholder="Your Message ..." onChange={onContentChange} value={content} maxLength={205}/>
                             </div>
-                            <button type="submit" onClick={onSendMessage}>Send</button>
+                            <button type="submit" onClick={onSendMessage} className="submit-message">Pin</button>
                         </form>
                     </div>
                 </div>
