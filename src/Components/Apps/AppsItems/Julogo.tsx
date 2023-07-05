3f87cc653f02320cdcaef85304bed0b944c17e35
Jupi <spacejupiter0@gmail.com>
2023-07-05 22:11:03 +0300

    added logo component

---
+++ src/Components/Apps/AppsItems/Julogo.tsx
@@ -0,0 +1,18 @@
+import "../../../Styles/Apps/Julogo.css";
+
+function Julogo()
+{
+    return <div className="logo">
+        <div className="first-line">
+            <div className="cloud-tall"></div>
+            <div className="cloud-organizer">
+                <div className="cloud-1 stickycloud"></div>
+                <div className="cloud-1"></div>
+            </div>
+        </div>
+        <div className="second-line"></div>
+        <div className="third-line"><div className="storm"></div></div>
+    </div>
+}
+
+export default Julogo;
