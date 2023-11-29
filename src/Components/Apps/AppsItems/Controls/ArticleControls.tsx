3211ba3935aebaeacac8177dfa387f319c8cdc46
Jupi <spacejupiter0@gmail.com>
2023-11-29 12:43:50 +0200

    Added articles control unit

---
+++ src/Components/Apps/AppsItems/Controls/ArticleControls.tsx
@@ -0,0 +1,56 @@
+import TextEditor from "../TextEditor";
+import "../../../../Styles/Apps/Controls.css";
+import { useState } from "react";
+import { createArticle } from "../../../../API/BlogList";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../Storage/Store";
+
+function ArticleControls()
+{
+    const [articleTextContent, setArticleTextContent] = useState<string>("");
+    const [titleContent, setTitleContent] = useState<string>("");
+    const [descriptionContent, setDescriptionContent] = useState<string>("");
+    const ControlsState = useSelector((x: RootState) => x.controlsState);
+
+    function onTitleChange(event: React.ChangeEvent<HTMLInputElement>)
+    {
+        event.preventDefault();
+        setTitleContent(event.target.value);
+    }
+
+    function onDescriptionChange(event: React.ChangeEvent<HTMLInputElement>)
+    {
+        event.preventDefault();
+        setDescriptionContent(event.target.value);
+    }
+
+    async function onCreateArticle()
+    {
+        const title = titleContent;
+        const description = descriptionContent;
+        const content = articleTextContent;
+        const password = ControlsState.password;
+        await createArticle(title, description, content, password);
+        // post here
+    }
+
+    return <div className="main-article-writing-container"> 
+        <div className="article-writing-section">
+            <div className="meta-data-container">
+                <div className="input-combo-container">
+                    <label>Title</label>
+                    <input className="article-input-field" value={titleContent} placeholder="Changing title with...." onChange={(onTitleChange)}/>
+                </div>
+                <div className="input-combo-container">
+                    <label>Description</label>
+                    <input className="article-input-field" placeholder="This article talks about...." value={descriptionContent} onChange={onDescriptionChange}/>
+                </div>
+            </div>
+
+            <TextEditor textContent={articleTextContent} updateTextContentFunction={setArticleTextContent}/>
+            <button type="button" className="create-article-button" onClick={onCreateArticle}>Create Article</button>
+        </div>
+    </div>
+}
+
+export default ArticleControls;
---
+++ src/Components/Apps/AppsItems/TextEditor.tsx
@@ -0,0 +1,52 @@
+import hljs from "highlight.js/lib/core";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
+import javascript from "highlight.js/lib/languages/javascript";
+import java from "highlight.js/lib/languages/java";
+import css from "highlight.js/lib/languages/css";
+hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage("java", java);
+hljs.registerLanguage("css", css);
+
+type propTypes = {
+    textContent: string,
+    updateTextContentFunction: (text: string) => void
+}
+
+function TextEditor(props: propTypes)
+{
+    hljs.configure({
+        languages: ['javascript', 'ruby', 'python', 'rust'],
+    });
+
+    const modules: any = {
+        toolbar: [
+          [{ 'header': [1, 2, false] }],
+          ['bold', 'italic', 'underline','strike', 'blockquote'],
+          [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+          ['link', 'image', 'code-block'],
+          ['clean'],
+        ],
+    }
+    
+    const formats: any = [
+        'header',
+        'bold', 'italic', 'underline', 'strike', 'blockquote',
+        'list', 'bullet', 'indent',
+        'link', 'image', 'code-block'
+    ]
+
+    function onUpdateContent(value: string)
+    {
+        props.updateTextContentFunction(value);
+    }
+
+    return <div className="post-text-editor">
+        <div className="ql-snow">
+            <ReactQuill className='react-quill-item' theme={"snow"} value={props.textContent} onChange={onUpdateContent} placeholder='content' formats={formats} modules={modules}/>
+        </div>
+    </div>
+}
+
+export default TextEditor;
