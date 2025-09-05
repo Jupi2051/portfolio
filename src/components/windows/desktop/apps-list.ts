d5918c617875e744c49919469256b9678ffe91cf
Jupi <spacejupiter0@gmail.com>
2025-09-06 01:10:57 +0300

    added artifical delay

--- src/components/windows/desktop/apps-list.ts
+++ src/components/windows/desktop/apps-list.ts
@@ -18,15 +18,21 @@ export enum DesktopAppsList {
 const handleFailedLoad = () => ({ default: AppLoadingFailed });
 
 export const DesktopAppsComponents: Record<DesktopAppsList, any> = {
-  [DesktopAppsList.DummyApp]: lazy(() => import("@/components/apps/dummy-app").catch(handleFailedLoad)),
-  [DesktopAppsList.Oni]: lazy(() => import("@/components/apps/oni").catch(handleFailedLoad)),
-  [DesktopAppsList.Jenni]: lazy(() => import("@/components/apps/jenni").catch(handleFailedLoad)),
-  [DesktopAppsList.EIEN]: lazy(() => import("@/components/apps/eien").catch(handleFailedLoad)),
-  [DesktopAppsList.GalaxyGym]: lazy(() => import("@/components/apps/galaxy-gym").catch(handleFailedLoad)),
+  [DesktopAppsList.DummyApp]: lazy(() => delayForDemo(import("@/components/apps/dummy-app").catch(handleFailedLoad))),
+  [DesktopAppsList.Oni]: lazy(() => delayForDemo(import("@/components/apps/oni").catch(handleFailedLoad))),
+  [DesktopAppsList.Jenni]: lazy(() => delayForDemo(import("@/components/apps/jenni").catch(handleFailedLoad))),
+  [DesktopAppsList.EIEN]: lazy(() => delayForDemo(import("@/components/apps/eien").catch(handleFailedLoad))),
+  [DesktopAppsList.GalaxyGym]: lazy(() => delayForDemo(import("@/components/apps/galaxy-gym").catch(handleFailedLoad))),
   [DesktopAppsList.Blog]: lazy(() => import("@/components/apps/blog").catch(handleFailedLoad)),
-  [DesktopAppsList.Controls]: lazy(() => import("@/components/apps/controls").catch(handleFailedLoad)),
-  [DesktopAppsList.Social]: lazy(() => import("@/components/apps/social").catch(handleFailedLoad)),
-  [DesktopAppsList.AboutMe]: lazy(() => import("@/components/apps/about-me").catch(handleFailedLoad)),
-  [DesktopAppsList.Photos]: lazy(() => import("@/components/apps/photos").catch(handleFailedLoad)),
-  [DesktopAppsList.Explorer]: lazy(() => import("@/components/apps/explorer").catch(handleFailedLoad)),
+  [DesktopAppsList.Controls]: lazy(() => delayForDemo(import("@/components/apps/controls").catch(handleFailedLoad))),
+  [DesktopAppsList.Social]: lazy(() => delayForDemo(import("@/components/apps/social").catch(handleFailedLoad))),
+  [DesktopAppsList.AboutMe]: lazy(() => delayForDemo(import("@/components/apps/about-me").catch(handleFailedLoad))),
+  [DesktopAppsList.Photos]: lazy(() => delayForDemo(import("@/components/apps/photos").catch(handleFailedLoad))),
+  [DesktopAppsList.Explorer]: lazy(() => delayForDemo(import("@/components/apps/explorer").catch(handleFailedLoad))),
 };
+
+function delayForDemo(promise: Promise<any>) {
+  return new Promise(resolve => {
+    setTimeout(resolve, 1000);
+  }).then(() => promise);
+}
