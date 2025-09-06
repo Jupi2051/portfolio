a96581a7b4d5ee586d2aa4cdf7003202eb8a8286
Jupi <spacejupiter0@gmail.com>
2025-09-06 21:41:43 +0300

    Fix for sizing on phone

--- src/components/surface.tsx
+++ src/components/surface.tsx
@@ -4,7 +4,7 @@ import StartMenu from "@/components/windows/start-menu";
 
 function Surface() {
   return (
-    <div className="relative h-screen bg-cover grid grid-cols-1 grid-rows-[1fr_0.05fr] overflow-hidden isolate bg-[url('/Imgs/background.webp')] isolate">
+    <div className="relative h-dvh bg-cover grid grid-cols-1 grid-rows-[1fr_0.05fr] overflow-hidden isolate bg-[url('/Imgs/background.webp')] isolate">
       <Desktop className="z-10" />
       <StartMenu className="z-20" />
       <Taskbar className="z-30" />
