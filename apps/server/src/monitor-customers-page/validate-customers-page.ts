fb32e8e6df98fe2900c87a009bf3a51b0fa23a04
Jupi <spacejupiter0@gmail.com>
2026-01-27 14:53:40 +0200

    test aga

--- apps/server/src/monitor-customers-page/validate-customers-page.ts
+++ apps/server/src/monitor-customers-page/validate-customers-page.ts
@@ -45,11 +45,14 @@ const validateCustomersPage = async () => {
   `);
 
   await page.goto(customersPageUrl, { waitUntil: "load" });
+  console.log("page loaded");
+  console.log("finding canvas...");
   const theCanvas = page.locator("canvas[data-engine]");
   await theCanvas.waitFor({ state: "visible" });
-
+  console.log("canvas found and visible");
   await page.waitForTimeout(5 * 1000);
 
+  console.log("taking screenshot of canvas...");
   const dataUrl = await page.evaluate(() => {
     //@ts-ignore
     const canvas = document.querySelector(
@@ -58,30 +61,35 @@ const validateCustomersPage = async () => {
     ) as HTMLCanvasElement;
     return canvas ? canvas.toDataURL("image/jpeg", 0.05) : null;
   });
+  console.log("screenshot taken");
 
   if (!dataUrl) throw new Error("Canvas not found or toDataURL failed");
-
+  console.log("dataUrl found converting to base jpeg buffer");
   const base64Data = dataUrl.replace(/^data:image\/\w+;base64,/, "");
   const realtimeScreenshotData = Buffer.from(base64Data, "base64");
+  console.log("realtimeScreenshotData found");
+  // await fs.promises.writeFile(
+  //   path.join(__dirname, 'realtime-screenshot-dada-dodo.png'),
+  //   realtimeScreenshotData
+  // )
 
-  await fs.promises.writeFile(
-    path.join(__dirname, "realtime-screenshot-dada-dodo.png"),
-    realtimeScreenshotData,
-  );
-
+  console.log("closing browser");
   await browser.close();
+  console.log("browser closed");
 
   const spaceToCheckColors = {
     topLeft: { x: 150, y: 150 },
     bottomRight: { x: 200, y: 200 },
   };
 
+  console.log("converting realtimeScreenshotData to imageData");
   const imageData = await sharp(realtimeScreenshotData)
     .raw()
     .toBuffer({ resolveWithObject: true });
 
   let isColorFound = false;
 
+  console.log("checking for colors in the image");
   try {
     for (
       let y = spaceToCheckColors.topLeft.y;
@@ -94,8 +102,10 @@ const validateCustomersPage = async () => {
         x++
       ) {
         const color = await getPixelColor(imageData, x, y);
+        console.log("color", color);
         if (color.r >= 60 && color.g >= 60 && color.b >= 60) {
           isColorFound = true;
+          console.log("color found", color);
           break;
         }
       }
