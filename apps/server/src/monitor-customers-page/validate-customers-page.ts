06e53bfef8809694566fe98b5ae15ee2ab35ae86
Jupi <spacejupiter0@gmail.com>
2026-01-28 15:03:27 +0200

    headless falase test

--- apps/server/src/monitor-customers-page/validate-customers-page.ts
+++ apps/server/src/monitor-customers-page/validate-customers-page.ts
@@ -34,7 +34,7 @@ function getChromiumLaunchOptions(): {
 const validateCustomersPage = async () => {
   const launchOpts = getChromiumLaunchOptions();
   const browser = await chromium.launch({
-    headless: true,
+    headless: false,
     args: launchOpts.args,
     ...(launchOpts.ignoreDefaultArgs && {
       ignoreDefaultArgs: launchOpts.ignoreDefaultArgs,
