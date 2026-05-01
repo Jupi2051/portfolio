c12207a35d3fd9de05cfd0db8593a14996753bf3
Jupi <spacejupiter0@gmail.com>
2026-05-01 16:31:11 +0300

    Refactor DesktopIcon component to enhance name display logic based on selection state, improving visual clarity and user interaction.

--- apps/web/src/components/windows/desktop/desktop-icon.tsx
+++ apps/web/src/components/windows/desktop/desktop-icon.tsx
@@ -131,7 +131,7 @@ function DesktopIcon(Props: PropTypes) {
         "relative flex flex-col w-[90px] h-[90px] pb-4 items-center justify-center text-white select-none isolate",
         'after:content-[" "] after:pointer-events-none after:w-[98%] after:h-full after:absolute after:left-1/2 after:top-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-md after:bg-transparent after:z-[-1] after:blur-lg after:transition-opacity after:duration-200 hover:after:bg-[#FFF]/25 active:after:opacity-0',
         {
-          "after:bg-linear-to-tl after:from-[#FFF]/30 after:to-[#FFF]/30 after:blur-lg":
+          "z-999 after:bg-linear-to-tl after:from-[#FFF]/30 after:to-[#FFF]/30 after:blur-lg":
             Props.Selected,
         },
       )}
@@ -168,7 +168,22 @@ function DesktopIcon(Props: PropTypes) {
         />
       ) : (
         <h1
-          className="absolute top-9/12 w-full px-1 font-normal text-xs mt-1.5 select-none uppercase text-center font-roboto-condensed break-all"
+          className={cn(
+            "absolute top-9/12 w-full px-1 font-normal text-xs mt-1.5 select-none uppercase text-center font-roboto-condensed break-all",
+            {
+              "overflow-visible": Props.Selected,
+              "overflow-hidden": !Props.Selected,
+            },
+          )}
+          style={
+            Props.Selected
+              ? undefined
+              : {
+                  display: "-webkit-box",
+                  WebkitLineClamp: 2,
+                  WebkitBoxOrient: "vertical",
+                }
+          }
           onMouseDown={onMouseDownName}
           onClick={onClickName}
         >
