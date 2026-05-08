e9054e25e46f375fa49435a4d4e53d66a04baeee
Jupi <spacejupiter0@gmail.com>
2026-05-08 22:44:13 +0300

    Refactor Intro component to improve code readability and remove unused WavyText component. Update background gradient class for consistency.

--- apps/web/src/components/intro/index.tsx
+++ apps/web/src/components/intro/index.tsx
@@ -1,7 +1,7 @@
-import { motion } from "framer-motion";
-import Julogo from "@/components/apps/about-me/jupi-logo";
-import OrbitLoader from "@/components/ui/orbit-loader";
-import WavyText from "@/components/ui/wavy-text";
+import { motion } from "framer-motion"
+import Julogo from "@/components/apps/about-me/jupi-logo"
+import OrbitLoader from "@/components/ui/orbit-loader"
+import WavyText from "@/components/ui/wavy-text"
 
 const Intro = ({ setLoaded }: { setLoaded: (loaded: boolean) => void }) => {
   return (
@@ -14,7 +14,7 @@ const Intro = ({ setLoaded }: { setLoaded: (loaded: boolean) => void }) => {
       }}
     >
       <motion.div
-        className="w-full h-full flex items-center justify-center bg-gradient-to-br bg-black"
+        className="w-full h-full flex items-center justify-center bg-linear-to-br bg-black"
         transition={{ delay: 0.5, ease: "easeInOut" }}
         initial={{ scale: 1.2, opacity: 0, filter: "blur(10px)" }}
         animate={{ scale: 1, opacity: 1, filter: "blur(0px)" }}
@@ -33,16 +33,16 @@ const Intro = ({ setLoaded }: { setLoaded: (loaded: boolean) => void }) => {
             />
             <OrbitLoader className="absolute top-1/2" moonClassName="!w-4" />
           </div>
-          <WavyText
+          {/* <WavyText
             text="Welcome"
             replay={true}
             className="text-4xl px-2 font-caveat absolute top-full translate-y-full pt-2"
             delay={0.8}
-          />
+          /> */}
         </div>
       </motion.div>
     </motion.div>
-  );
-};
+  )
+}
 
-export default Intro;
+export default Intro
