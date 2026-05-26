1af9d1dc8a9ca1a0f17d263c78f4ac1fbaf6e04f
Jupi <spacejupiter0@gmail.com>
2026-05-26 23:41:48 +0300

    Refactor TaskBarApp component for improved readability and consistency

--- apps/web/src/components/windows/taskbar/taskbar-app.tsx
+++ apps/web/src/components/windows/taskbar/taskbar-app.tsx
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { motion, Variants } from "framer-motion";
-import { useDispatch } from "react-redux";
-import { setMinimizedState } from "@/storage/slices/desktop";
-import cn from "classnames";
-import useAppWindowData from "@/hooks/use-app-window-data";
-import useStartMenu from "@/hooks/use-start-menu";
-import useGlobalWindowsControls from "@/hooks/use-global-windows-controls";
+import React, { useState, useEffect } from "react"
+import { motion, Variants } from "framer-motion"
+import { useDispatch } from "react-redux"
+import { setMinimizedState } from "@/storage/slices/desktop"
+import cn from "classnames"
+import useAppWindowData from "@/hooks/use-app-window-data"
+import useStartMenu from "@/hooks/use-start-menu"
+import useGlobalWindowsControls from "@/hooks/use-global-windows-controls"
 
 type PropTypes = {
-  Icon: string;
-  HideStatusBar?: boolean;
-  isWindowsIcon?: boolean;
-  isCustomIcon?: boolean;
-  AppId?: number;
-  forceHover?: boolean;
-  imageClassName?: string;
-};
+  Icon: string
+  HideStatusBar?: boolean
+  isWindowsIcon?: boolean
+  isCustomIcon?: boolean
+  AppId?: number
+  forceHover?: boolean
+  imageClassName?: string
+}
 
 const AnimationFrames: Variants = {
   init: { y: 50 },
@@ -37,66 +37,66 @@ const AnimationFrames: Variants = {
   },
   tap: { scale: 0.825 },
   exit: { y: 100, transition: { ease: "easeIn", duration: 0.2 } },
-};
+}
 
 function TaskBarApp(Props: PropTypes) {
   const { isMinimized, isFocused, focusWindow, bringWindowToFront } =
-    useAppWindowData(Props.AppId);
-  const { unFocusAllWindows } = useGlobalWindowsControls();
-  const [isOpening, setIsOpening] = useState<boolean>();
-  const [isInitialRender, setIsInitialRender] = useState(true);
-  const { isRendered, setRenderStartMenu } = useStartMenu();
-  const dispatch = useDispatch();
+    useAppWindowData(Props.AppId)
+  const { unFocusAllWindows } = useGlobalWindowsControls()
+  const [isOpening, setIsOpening] = useState<boolean>()
+  const [isInitialRender, setIsInitialRender] = useState(true)
+  const { isRendered, setRenderStartMenu } = useStartMenu()
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsInitialRender(false);
-    }, 300);
-    return () => clearTimeout(timer);
-  }, []);
+      setIsInitialRender(false)
+    }, 300)
+    return () => clearTimeout(timer)
+  }, [])
 
   const OnClickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
     if (Props.isWindowsIcon || Props.isCustomIcon) {
-      unFocusAllWindows();
-      if (Props.isWindowsIcon) setRenderStartMenu(!isRendered);
-      return;
+      unFocusAllWindows()
+      if (Props.isWindowsIcon) setRenderStartMenu(!isRendered)
+      return
     }
 
-    event.preventDefault();
+    event.preventDefault()
 
     if (Props.AppId) {
       if (!isMinimized && !isFocused) {
-        bringWindowToFront();
-        focusWindow();
-        setIsOpening(undefined);
+        bringWindowToFront()
+        focusWindow()
+        setIsOpening(undefined)
       } else {
-        const MinimizedState = !isMinimized;
+        const MinimizedState = !isMinimized
         dispatch(
           setMinimizedState({
             id: Props.AppId,
             state: MinimizedState,
-          })
-        );
+          }),
+        )
 
         if (!MinimizedState) {
-          focusWindow();
-          bringWindowToFront();
-        } else unFocusAllWindows();
+          focusWindow()
+          bringWindowToFront()
+        } else unFocusAllWindows()
 
-        setIsOpening(!MinimizedState);
+        setIsOpening(!MinimizedState)
       }
     }
-  };
+  }
 
   return (
     <motion.button
       className={cn(
-        "w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center relative select-none sm:px-[2rem] group",
+        "w-10 h-10 sm:w-12 sm:h-12 flex items-center justify-center relative select-none sm:px-8 group",
         "before:content-[''] before:block before:absolute before:top-1/2 before:left-1/2 before:opacity-0 before:bg-white/5 before:w-9 before:h-9 sm:before:w-[3.6rem] sm:before:h-[50px] before:rounded-md before:border-transparent before:border before:pointer-events-none before:-translate-1/2 before:scale-90 hover:before:scale-100 hover:before:opacity-100 before:transition-all before:duration-100 before:ease-in-out hover:before:border-white/5",
         {
-          "before:!border-white/5 before:!scale-100 before:!opacity-100":
+          "before:border-white/5! before:scale-100! before:opacity-100!":
             Props.forceHover || isFocused,
-        }
+        },
       )}
       onClick={OnClickHandler}
       whileTap={"tap"}
@@ -113,20 +113,20 @@ function TaskBarApp(Props: PropTypes) {
         variants={AnimationFrames}
         src={Props.Icon}
         className={cn(
-          "max-w-6 max-h-9 sm:max-w-10 sm:max-h-14 pointer-events-none",
+          "max-w-6 max-h-9 sm:max-w-10 sm:max-h-10 pointer-events-none",
           Props.imageClassName,
           {
             "group-active:rounded-md group-active:filter-[hue-rotate(10deg)_brightness(80%)_saturate(3.5)]":
               Props.isWindowsIcon,
-          }
+          },
         )}
       />
       <motion.span
         className={cn(
-          "absolute block bottom-0 -translate-y-full sm:translate-y-0 sm:top-full sm:bottom-0 left-1/2 -translate-x-1/2 bg-[#93909f] rounded-md h-[2.9px] sm:h-[3.34px] -mt-px border-transparent pointer-events-none",
+          "absolute block bottom-0 -translate-y-full sm:-translate-y-px sm:top-full sm:bottom-0 left-1/2 -translate-x-1/2 bg-[#93909f] rounded-md h-[2.9px] sm:h-[3.34px] -mt-px border-transparent pointer-events-none",
           {
             hidden: Props.HideStatusBar,
-          }
+          },
         )}
         initial={{
           width: "0px",
@@ -140,7 +140,7 @@ function TaskBarApp(Props: PropTypes) {
         }}
       />
     </motion.button>
-  );
+  )
 }
 
-export default TaskBarApp;
+export default TaskBarApp
