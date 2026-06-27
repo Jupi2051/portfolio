4621537dfbd0178813e59e7a0e84073204494999
Jupi <spacejupiter0@gmail.com>
2026-06-27 19:10:02 +0300

    use datefns

--- apps/web/src/components/windows/taskbar/background-apps/date-time.tsx
+++ apps/web/src/components/windows/taskbar/background-apps/date-time.tsx
@@ -1,27 +1,27 @@
-import moment from "moment";
-import { useEffect, useState } from "react";
-import BackgroundAppWrapper from "./background-app-wrapper";
+import { format } from "date-fns"
+import { useEffect, useState } from "react"
+import BackgroundAppWrapper from "./background-app-wrapper"
 
 function DateTime() {
-  const [DateTime, SetDateTime] = useState(new Date());
+  const [DateTime, SetDateTime] = useState(new Date())
 
   useEffect(() => {
     const SecondTimer = setInterval(() => {
-      SetDateTime(new Date());
-    });
-    return () => clearInterval(SecondTimer);
-  }, []);
+      SetDateTime(new Date())
+    })
+    return () => clearInterval(SecondTimer)
+  }, [])
 
   return (
     <BackgroundAppWrapper>
-      <div className="text-white font-segoe-ui flex-col items-center text-sm -tracking-tight select-none">
+      <div className="text-white font-segoe-ui flex-col items-center text-sm tracking-wide select-none">
         <p className="text-right select-none">
-          {moment(DateTime).format("LT")}
+          {format(DateTime, "p")}
         </p>
-        <p className="text-right select-none">{moment(DateTime).format("l")}</p>
+        <p className="text-right select-none">{format(DateTime, "P")}</p>
       </div>
     </BackgroundAppWrapper>
-  );
+  )
 }
 
-export default DateTime;
+export default DateTime
