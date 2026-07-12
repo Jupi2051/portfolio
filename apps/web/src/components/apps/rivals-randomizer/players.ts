b8f02ad51f6b086112eafec8e86e6b956c41cd7b
Jupi <spacejupiter0@gmail.com>
2026-07-12 22:10:43 +0300

    update

--- apps/web/src/components/apps/rivals-randomizer/players.ts
+++ apps/web/src/components/apps/rivals-randomizer/players.ts
@@ -1,17 +1,21 @@
-import bonnie from "@/assets/rivals-players/bonnie.png"
-import burger from "@/assets/rivals-players/burger.png"
-import filip from "@/assets/rivals-players/filip.png"
-import jenni from "@/assets/rivals-players/jenni.gif"
-// import chonkymouse from "@/assets/rivals-players/chonkymouse.png"
+// import bonnie from "@/assets/rivals-players/bonnie.png"
+// import burger from "@/assets/rivals-players/burger.png"
+// import filip from "@/assets/rivals-players/filip.png"
+// import jenni from "@/assets/rivals-players/jenni.gif"
+import chonkymouse from "@/assets/rivals-players/chonkymouse.png"
 import jupi from "@/assets/rivals-players/jupi.png"
 import kek from "@/assets/rivals-players/kek.png"
-// import mimi from "@/assets/rivals-players/mimi.png"
-import viggo from "@/assets/rivals-players/viggo.webp"
+import mimi from "@/assets/rivals-players/mimi.png"
+// import viggo from "@/assets/rivals-players/viggo.webp"
 import n0rieth from "@/assets/rivals-players/noreith.png"
 import oranthur from "@/assets/rivals-players/oranthur.png"
-import sage from "@/assets/rivals-players/sage.png"
-import thomas from "@/assets/rivals-players/thomas.webp"
+// import sage from "@/assets/rivals-players/sage.png"
+import shonareru from "@/assets/rivals-players/shonareru.png"
+// import thomas from "@/assets/rivals-players/thomas.webp"
 import yuu from "@/assets/rivals-players/yuu.png"
+import vaeel from "@/assets/rivals-players/Vaeel.webp"
+import andrespirat from "@/assets/rivals-players/Andrespirat.webp"
+import lownex from "@/assets/rivals-players/Lownex.webp"
 import type { RivalsPlayer } from "./types"
 
 export const RIVALS_PLAYERS: RivalsPlayer[] = [
@@ -27,42 +31,42 @@ export const RIVALS_PLAYERS: RivalsPlayer[] = [
       { weight: 0.6, role: 1 },
     ],
   },
-  {
-    id: "filip",
-    name: "Filip",
-    image: filip,
-    peakRank: { label: "Celestial 1", skillLevel: 9 },
-    skillLevel: 1,
-    roleSkills: [
-      { weight: 1.0, role: 2 },
-      { weight: 0.8, role: 1 },
-      { weight: 0.6, role: 0 },
-    ],
-  },
-  {
-    id: "bonnie",
-    name: "Bonnie",
-    image: bonnie,
-    peakRank: { label: "Celestial 2", skillLevel: 8 },
-    skillLevel: 0.95,
-    roleSkills: [
-      { weight: 0.95, role: 0 },
-      { weight: 0.65, role: 1 },
-      { weight: 0.6, role: 2 },
-    ],
-  },
-  {
-    id: "thomas",
-    name: "DonnerGX",
-    image: thomas,
-    peakRank: { label: "Silver 3", skillLevel: 0 },
-    skillLevel: 0.1,
-    roleSkills: [
-      { weight: 0.1, role: 1 },
-      { weight: 0.08, role: 0 },
-      { weight: 0.05, role: 2 },
-    ],
-  },
+  // {
+  //   id: "filip",
+  //   name: "Filip",
+  //   image: filip,
+  //   peakRank: { label: "Celestial 1", skillLevel: 9 },
+  //   skillLevel: 1,
+  //   roleSkills: [
+  //     { weight: 1.0, role: 2 },
+  //     { weight: 0.8, role: 1 },
+  //     { weight: 0.6, role: 0 },
+  //   ],
+  // },
+  // {
+  //   id: "bonnie",
+  //   name: "Bonnie",
+  //   image: bonnie,
+  //   peakRank: { label: "Celestial 2", skillLevel: 8 },
+  //   skillLevel: 0.95,
+  //   roleSkills: [
+  //     { weight: 0.95, role: 0 },
+  //     { weight: 0.65, role: 1 },
+  //     { weight: 0.6, role: 2 },
+  //   ],
+  // },
+  // {
+  //   id: "thomas",
+  //   name: "DonnerGX",
+  //   image: thomas,
+  //   peakRank: { label: "Silver 3", skillLevel: 0 },
+  //   skillLevel: 0.1,
+  //   roleSkills: [
+  //     { weight: 0.1, role: 1 },
+  //     { weight: 0.08, role: 0 },
+  //     { weight: 0.05, role: 2 },
+  //   ],
+  // },
   {
     id: "n0rieth",
     name: "N0rieth",
@@ -87,18 +91,18 @@ export const RIVALS_PLAYERS: RivalsPlayer[] = [
       { weight: 0.5, role: 0 },
     ],
   },
-  {
-    id: "burger",
-    name: "Burger Reviewer",
-    image: burger,
-    peakRank: { label: "Grandmaster 2", skillLevel: 5 },
-    skillLevel: 0.75,
-    roleSkills: [
-      { weight: 0.85, role: 1 },
-      { weight: 0.7, role: 2 },
-      { weight: 0.3, role: 0 },
-    ],
-  },
+  // {
+  //   id: "burger",
+  //   name: "Burger Reviewer",
+  //   image: burger,
+  //   peakRank: { label: "Grandmaster 2", skillLevel: 5 },
+  //   skillLevel: 0.75,
+  //   roleSkills: [
+  //     { weight: 0.85, role: 1 },
+  //     { weight: 0.7, role: 2 },
+  //     { weight: 0.3, role: 0 },
+  //   ],
+  // },
   {
     id: "oranthur",
     name: "Oranthur",
@@ -111,10 +115,22 @@ export const RIVALS_PLAYERS: RivalsPlayer[] = [
       { weight: 0.5, role: 1 },
     ],
   },
+  // {
+  //   id: "sage",
+  //   name: "Sage",
+  //   image: sage,
+  //   peakRank: { label: "Platinum 3", skillLevel: 2 },
+  //   skillLevel: 0.45,
+  //   roleSkills: [
+  //     { weight: 0.45, role: 2 },
+  //     { weight: 0.45, role: 0 },
+  //     { weight: 0.1, role: 1 },
+  //   ],
+  // },
   {
-    id: "sage",
-    name: "Sage",
-    image: sage,
+    id: "shonareru",
+    name: "Shonareru",
+    image: shonareru,
     peakRank: { label: "Platinum 3", skillLevel: 2 },
     skillLevel: 0.45,
     roleSkills: [
@@ -123,47 +139,23 @@ export const RIVALS_PLAYERS: RivalsPlayer[] = [
       { weight: 0.1, role: 1 },
     ],
   },
-  {
-    id: "jenni",
-    name: "Jenni",
-    image: jenni,
-    peakRank: { label: "Diamond 2", skillLevel: 1 },
-    skillLevel: 0.3,
-    roleSkills: [
-      { weight: 0.4, role: 0 },
-      { weight: 0.3, role: 1 },
-      { weight: 0.1, role: 2 },
-    ],
-  },
   // {
-  //   id: "chonkymouse",
-  //   name: "ChonkyMouse",
-  //   image: chonkymouse,
-  //   peakRank: { label: "Silver 3", skillLevel: 0.5 },
-  //   skillLevel: 0.1,
-  //   roleSkills: [
-  //     { weight: 0.1, role: 1 },
-  //     { weight: 0.08, role: 0 },
-  //     { weight: 0.05, role: 2 },
-  //   ],
-  // },
-  // {
-  //   id: "mimi",
-  //   name: "Mimi",
-  //   image: mimi,
-  //   peakRank: { label: "Gold 3", skillLevel: 1 },
+  //   id: "jenni",
+  //   name: "Jenni",
+  //   image: jenni,
+  //   peakRank: { label: "Diamond 2", skillLevel: 1 },
   //   skillLevel: 0.3,
   //   roleSkills: [
-  //     { weight: 0.5, role: 0 },
-  //     { weight: 0.4, role: 1 },
-  //     { weight: 0.3, role: 2 },
+  //     { weight: 0.4, role: 0 },
+  //     { weight: 0.3, role: 1 },
+  //     { weight: 0.1, role: 2 },
   //   ],
   // },
   {
-    id: "viggo",
-    name: "Viggo",
-    image: viggo,
-    peakRank: { label: "Silver 3", skillLevel: 0 },
+    id: "chonkymouse",
+    name: "ChonkyMouse",
+    image: chonkymouse,
+    peakRank: { label: "Silver 3", skillLevel: 0.5 },
     skillLevel: 0.1,
     roleSkills: [
       { weight: 0.1, role: 1 },
@@ -171,6 +163,30 @@ export const RIVALS_PLAYERS: RivalsPlayer[] = [
       { weight: 0.05, role: 2 },
     ],
   },
+  {
+    id: "mimi",
+    name: "Mimi",
+    image: mimi,
+    peakRank: { label: "Gold 3", skillLevel: 1 },
+    skillLevel: 0.3,
+    roleSkills: [
+      { weight: 0.5, role: 0 },
+      { weight: 0.4, role: 1 },
+      { weight: 0.3, role: 2 },
+    ],
+  },
+  // {
+  //   id: "viggo",
+  //   name: "Viggo",
+  //   image: viggo,
+  //   peakRank: { label: "Silver 3", skillLevel: 0 },
+  //   skillLevel: 0.1,
+  //   roleSkills: [
+  //     { weight: 0.1, role: 1 },
+  //     { weight: 0.08, role: 0 },
+  //     { weight: 0.05, role: 2 },
+  //   ],
+  // },
   {
     id: "yuu",
     name: "Yuu",
@@ -181,6 +197,42 @@ export const RIVALS_PLAYERS: RivalsPlayer[] = [
       { weight: 1.0, role: 0 },
       { weight: 0.45, role: 1 },
       { weight: 0.4, role: 2 },
+    ],
+  },
+  {
+    id: "vaeel",
+    name: "Vaeel",
+    image: vaeel,
+    peakRank: { label: "Grandmaster 1", skillLevel: 6 },
+    skillLevel: 0.9,
+    roleSkills: [
+      { weight: 0.9, role: 2 },
+      { weight: 0.7, role: 0 },
+      { weight: 0.55, role: 1 },
+    ],
+  },
+  {
+    id: "andrespirat",
+    name: "Andrespirat",
+    image: andrespirat,
+    peakRank: { label: "Celestial 1", skillLevel: 9 },
+    skillLevel: 1,
+    roleSkills: [
+      { weight: 1.0, role: 2 },
+      { weight: 0.8, role: 1 },
+      { weight: 0.6, role: 0 },
+    ],
+  },
+  {
+    id: "lownex",
+    name: "Lownex",
+    image: lownex,
+    peakRank: { label: "Grandmaster 1", skillLevel: 6 },
+    skillLevel: 0.9,
+    roleSkills: [
+      { weight: 0.9, role: 0 },
+      { weight: 0.7, role: 2 },
+      { weight: 0.55, role: 1 },
     ],
   },
 ]
